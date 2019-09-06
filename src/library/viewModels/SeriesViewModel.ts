63a0edf69e042985d673ebacd8d73396bf6c5ad0
Deathspike <meister.deathspike@outlook.com>
2019-09-06 20:35:28 +0200

    Chapter synchronization, delete, and progress

--- src/library/viewModels/SeriesViewModel.ts
+++ src/library/viewModels/SeriesViewModel.ts
@@ -7,9 +7,9 @@ import {language} from '../language';
 export class SeriesViewModel {
   private readonly _context: app.ContextApi;
 
-  constructor(series: app.ILibrarySeries) {
+  constructor(series: app.ILibrarySeries, sessionList: app.ISessionList) {
     this._context = app.core.service.get(app.settings.contextKey);
-    this._updateWith(series);
+    this._updateWith(series, sessionList);
   }
   
   @mobx.action
@@ -24,15 +24,23 @@ export class SeriesViewModel {
   }
 
   @mobx.action
-  async refreshAsync() {
-    this.isLoading = true;
-    const series = await this._context.library.seriesReadAsync(this.id);
-    if (series.value) {
-      this._updateWith(series.value);
+  async refreshAsync(userInitiated = true) {
+    this.isLoading = userInitiated;
+    const seriesPromise = this._context.library.seriesReadAsync(this.id);
+    const sessionListPromise = this._context.session.listAsync(this.id);
+    const series = await seriesPromise;
+    const sessionList = await sessionListPromise;
+    if (series.value && sessionList.value) {
+      this._updateWith(series.value, sessionList.value);
       this.isLoading = false;
-    } else if (await app.core.dialog.errorAsync(true, series.error)) {
+    } else if (userInitiated && await app.core.dialog.errorAsync(true, series.error, sessionList.error)) {
       await this.refreshAsync();
     }
+  }
+
+  @mobx.action
+  async repeatAsync() {
+    this.refreshAsync(false);
   }
 
   @mobx.observable
@@ -56,17 +64,25 @@ export class SeriesViewModel {
   @mobx.observable
   title!: string;
 
-  private _updateWith(series: app.ILibrarySeries) {
-    this.chapters = series.chapters.map(this._viewModelFor.bind(this));
+  private _updateWith(series: app.ILibrarySeries, sessionList: app.ISessionList) {
     this.id = series.id;
     this.image = series.source.image;
     this.summary = series.source.summary;
     this.title = series.source.title;
+    this.chapters = series.chapters.map((chapter) => this._viewModelFor(chapter, sessionList));
   }
 
-  private _viewModelFor(chapter: app.ILibrarySeriesChapter) {
+  private _viewModelFor(chapter: app.ILibrarySeriesChapter, sessionList: app.ISessionList) {
     const vm = this.chapters && this.chapters.find((vm) => chapter.id === vm.id);
-    if (vm) return vm.refreshWith(chapter);
-    return new app.ChapterViewModel(this._context, this, chapter)
+    if (vm) return vm.refreshWith(chapter, checkIsSynchronizing(this.id, chapter, sessionList));
+    return new app.ChapterViewModel(this._context, this, chapter, checkIsSynchronizing(this.id, chapter, sessionList));
   }
+}
+
+function checkIsSynchronizing(seriesId: string, chapter: app.ILibrarySeriesChapter, sessionList: app.ISessionList) {
+  return sessionList.some((session) => !session.finishedAt
+    && session.library
+    && session.library.seriesId === seriesId
+    && session.library.chapterId === chapter.id
+    && session.library.sync)
 }
