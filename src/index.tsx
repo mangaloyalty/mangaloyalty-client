4426fc6335165f117255ebe97ebf110c13c253a6
Deathspike <meister.deathspike@outlook.com>
2019-09-27 22:25:25 +0200

    Import `tslib` to reduce production bundle size

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-// UX: Generic: Use `tslib` helper for better built package sizes.
 // UX: Generic: Production-ready lazy loading library for performance-heavy lists (library/chapters). Optimize tooltips?
 // UX: Generic: EmptyComponent does not respect parent claimed tab space.
 // UX: Reader: Option to disable image processing (e.g. old series are parsed too aggressively).
