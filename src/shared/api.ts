37164192fdfa6b0e5f81182bdb49d28c083f1541
Deathspike <>
2019-10-05 12:04:57 +0200

    Add websocket connection and websocket utilities

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -2,27 +2,37 @@ import * as app from '.';
 let cacheLibrary: app.ContextLibraryApi;
 let cacheRemote: app.ContextRemoteApi;
 let cacheSession: app.ContextSessionApi;
+let cacheSocket: app.ContextSocketApi;
 
 export const api = {
   get library() {
     if (cacheLibrary) return cacheLibrary;
-    cacheLibrary = new app.ContextLibraryApi(createHttp());
+    cacheLibrary = new app.ContextLibraryApi(new app.HttpApi(createUrl()));
     return cacheLibrary;
   },
 
   get remote() {
     if (cacheRemote) return cacheRemote;
-    cacheRemote = new app.ContextRemoteApi(createHttp());
+    cacheRemote = new app.ContextRemoteApi(new app.HttpApi(createUrl()));
     return cacheRemote;
   },
 
   get session() {
     if (cacheSession) return cacheSession;
-    cacheSession = new app.ContextSessionApi(createHttp());
+    cacheSession = new app.ContextSessionApi(new app.HttpApi(createUrl()));
     return cacheSession;
+  },
+
+  get socket() {
+    if (cacheSocket) return cacheSocket;
+    cacheSocket = new app.ContextSocketApi(createUrl());
+    return cacheSocket;
   }
 };
 
-function createHttp() {
-  return new app.HttpApi(`${location.protocol}//${location.hostname}:${location.port === '7767' ? '7783' : location.port}`);
+function createUrl() {
+  const protocol = location.protocol;
+  const hostname = location.hostname;
+  const port = location.port === '7767' ? '7783' : location.port;
+  return `${protocol}//${hostname}:${port}`;
 }
---
+++ src/shared/components/SocketComponent.tsx
@@ -0,0 +1,30 @@
+import * as app from '..';
+import * as mui from '@material-ui/core';
+import * as React from 'react';
+
+export class SocketComponent extends React.Component<{queue: app.ContextSocketQueue, onActionAsync: (actions: app.ISocketAction[]) => Promise<void>}> {
+  componentDidMount() {
+    this.props.queue.attach();
+    this.props.queue.register((actions) => this._runAsync(actions));
+  }
+
+  componentWillUnmount() {
+    this.props.queue.detach();
+  }
+
+  render() {
+    return (
+      <mui.Grid>
+        {this.props.children}
+      </mui.Grid>
+    );
+  }
+  
+  private async _runAsync(actions: app.ISocketAction[]) {
+    try {
+      await this.props.onActionAsync(actions);
+    } catch (error) {
+      await app.core.dialog.errorAsync(() => this._runAsync(actions), error);
+    }
+  }
+}
