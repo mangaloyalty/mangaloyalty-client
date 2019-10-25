4d83980491411f711f5532c6f55cc0dee4cd6b07
Deathspike <>
2019-10-25 15:51:07 +0200

    Split page tracking to minimize read interruptions

--- src/session/viewModels/MainViewModel.ts
+++ src/session/viewModels/MainViewModel.ts
@@ -10,6 +10,7 @@ export class MainViewModel {
   private _imageNextTime?: number;
   private _imagePreviousTime?: number;
   private _pageNumber: number;
+  private _trackPromise: Promise<boolean>;
 
   constructor(navigator: app.INavigator, session: app.ISessionListItem, title: string, pageNumber?: number) {
     this._loader = new app.Loader(session);
@@ -17,6 +18,7 @@ export class MainViewModel {
     this._session = session;
     this._pageNumber = pageNumber || 1;
     this._title = title;
+    this._trackPromise = Promise.resolve(false);
   }
 
   @mobx.action
@@ -49,6 +51,7 @@ export class MainViewModel {
       this.showControls = false;
     } else if (this._pageNumber < this._session.pageCount) {
       this._pageNumber++;
+      this._queueTrack(this._pageNumber);
       await this.updateAsync();
     } else if (!this._navigator.hasNext) {
       app.core.toast.add(language.sessionToastNextUnavailable);
@@ -66,6 +69,7 @@ export class MainViewModel {
       this.showControls = false;
     } else if (this._pageNumber > 1) {
       this._pageNumber--;
+      this._queueTrack(this._pageNumber);
       await this.updateAsync();
     } else if (!this._navigator.hasPrevious) {
       app.core.toast.add(language.sessionToastPreviousUnavailable);
@@ -85,17 +89,12 @@ export class MainViewModel {
   @mobx.action
   async updateAsync() {
     await app.core.screen.loadAsync(async () => {
-      const sessionPagePromise = this._loader.getAsync(this._pageNumber); 
-      const trackPromise = this._navigator.trackAsync ? this._navigator.trackAsync(this._session.pageCount, this._pageNumber) : true;
-      const sessionPage = await sessionPagePromise;
+      const sessionPage = await this._loader.getAsync(this._pageNumber);
       if (sessionPage.value) {
         this.imageNode = sessionPage.value;
-        // TODO: The tracking API call can take ~90-400ms, causing a spinner. Off-load this call?
-        if (!await trackPromise) await app.core.dialog.errorAsync(() => this.updateAsync());
       } else if (sessionPage.status === 404) {
         await app.core.screen.leaveAsync();
       } else {
-        await trackPromise;
         await app.core.dialog.errorAsync(() => this.updateAsync(), sessionPage.error);
       }
     });
@@ -118,6 +117,12 @@ export class MainViewModel {
   
   @mobx.observable
   showControls = false;
+
+  private _queueTrack(pageNumber: number) {
+    if (!this._navigator.trackAsync) return;
+    const trackHandler = () => this._navigator.trackAsync!(this._session.pageCount, pageNumber);
+    this._trackPromise = this._trackPromise.then(trackHandler, trackHandler);
+  }
 }
 
 function checkActionLeave(actions: app.ISocketAction[], sessionId: string) {
