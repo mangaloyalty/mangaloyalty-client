d372e8e542abfb38e829f5c2b71b54ad83de587a
Deathspike <>
2019-10-28 10:07:26 +0100

    Add image lazy loading offset

--- src/shared/components/SeriesImageComponent.tsx
+++ src/shared/components/SeriesImageComponent.tsx
@@ -3,11 +3,11 @@ import * as app from '..';
 import * as mui from '@material-ui/core';
 import * as React from 'react';
 
-export class SeriesImage extends React.Component<{src: string, style?: React.CSSProperties}> {
+export class SeriesImage extends React.Component<{offset?: number, src: string, style?: React.CSSProperties}> {
   render() {
     return (
       <mui.Grid style={{...this.props.style, ...styles.container}}>
-        <LazyLoad height={'100%'} resize>
+        <LazyLoad height={'100%'} offset={this.props.offset} resize>
           <img src={this.props.src} style={styles.image}
             onError={(ev) => ev.currentTarget.style.opacity = '1'}
             onLoad={(ev) => ev.currentTarget.style.opacity = '1'} />
