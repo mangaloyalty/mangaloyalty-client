e2f9d0d151099c060816b147d77a2d9588414ce1
Roel van Uden <r.h.g.v.uden@outlook.com>
2019-11-15 19:58:55 +0100

    Add floating action buttons & show automate on add

--- src/shared/language.ts
+++ src/shared/language.ts
@@ -1,6 +1,4 @@
 export const language = {
-  addedBody: 'This item has been added to the library.',
-  addedButtons: ['OK'],
   deleteBody: 'You are about to delete this item from the server. Are you sure?',
   deleteButtons: ['Yes', 'No'],
   errorBody: 'The server could not be reached or returned an error.',
