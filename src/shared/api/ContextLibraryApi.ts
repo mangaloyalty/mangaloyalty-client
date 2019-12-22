caacb0f5faa1512ee3fb4b9fb5422f061571bc57
Deathspike <>
2019-12-22 09:45:34 +0100

    Remove the `findByUrl` API

--- src/shared/api/ContextLibraryApi.ts
+++ src/shared/api/ContextLibraryApi.ts
@@ -29,13 +29,6 @@ export class ContextLibraryApi {
     return response;
   }
   
-  async seriesFindByUrlAsync(url: string) {
-    const query = new app.HttpQuery().add('url', url);
-    const request = this._http.get(`${this._baseUrl}/api/library/findByUrl` + query);
-    const response = await request.json<app.ILibrarySeriesFindByUrlResponse>();
-    return response;
-  }
-
   async seriesImageDataAsync(seriesId: string) {
     const request = this._http.get(this.seriesImageUrl(seriesId));
     const response = await request.imageData();
