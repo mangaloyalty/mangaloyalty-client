7730e89b28a613ab72a9972c09b6cbbf2621aee2
Deathspike <>
2020-02-28 14:35:32 +0100

    Add swipe gesture support

--- src/session/enumerators/PageSize.ts
+++
@@ -1,8 +0,0 @@
-export enum PageSize {
-  None = 0,
-  Paperback = 1 / 112 * 176,
-  Digest = 1 / 140 * 210,
-  Pocket = 1 / 105 * 152,
-  JapaneseB6 = 1 / 128 * 182,
-  ISOA5 = 1 / 148 * 210
-}
---
+++ src/session/typings.d.ts
@@ -0,0 +1,19 @@
+export const enum DirectionType {
+  Up = 0,
+  Right = 1,
+  Down = 2,
+  Left = 3
+}
+
+export const enum PageSize {
+  None = 0,
+  Paperback = 1 / 112 * 176,
+  Digest = 1 / 140 * 210,
+  Pocket = 1 / 105 * 152,
+  JapaneseB6 = 1 / 128 * 182,
+  ISOA5 = 1 / 148 * 210
+}
+
+export type ITouchEvent =
+  {type: 'Swipe', direction: DirectionType} |
+  {type: 'Tap', x: number, y: number};
