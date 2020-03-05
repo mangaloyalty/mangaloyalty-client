bffe58691fa84d43cac7072970bd2fa0a4ff862d
Deathspike <>
2020-03-05 19:50:31 +0100

    Remove navigation options since they add no value

--- src/session/viewModels/MainSettingsViewModel.ts
+++ src/session/viewModels/MainSettingsViewModel.ts
@@ -15,49 +15,22 @@ export class MainSettingsViewModel {
   }
 
   @mobx.action
-  toggleNavigationKeyboard() {
-    this.enableNavigationKeyboard = !this.enableNavigationKeyboard;
-    localStorage.setItem('SessionNavigationKeyboard', String(this.enableNavigationKeyboard));
-  }
-
-  @mobx.action
-  toggleNavigationSwipe() {
-    this.enableNavigationSwipe = !this.enableNavigationSwipe;
-    localStorage.setItem('SessionNavigationSwipe', String(this.enableNavigationSwipe));
-  }
-
-  @mobx.action
-  toggleNavigationTap() {
-    this.enableNavigationTap = !this.enableNavigationTap;
-    localStorage.setItem('SessionNavigationTap', String(this.enableNavigationTap));
-  }
-
-  @mobx.action
   toggleOptionAutohide() {
-    this.enableOptionAutohide = !this.enableOptionAutohide;
-    localStorage.setItem('SessionOptionAutohide', String(this.enableOptionAutohide));
+    this.optionAutohide = !this.optionAutohide;
+    localStorage.setItem('SessionOptionAutohide', String(this.optionAutohide));
   }
 
   @mobx.action
   toggleOptionRTL() {
-    this.enableOptionRTL = !this.enableOptionRTL;
-    localStorage.setItem('SessionOptionRTL', String(this.enableOptionRTL));
+    this.optionRTL = !this.optionRTL;
+    localStorage.setItem('SessionOptionRTL', String(this.optionRTL));
   }
 
   @mobx.observable
-  enableNavigationKeyboard = localStorage.getItem('SessionNavigationKeyboard') !== 'false';
+  optionAutohide = localStorage.getItem('SessionOptionAutohide') === 'true';
 
   @mobx.observable
-  enableNavigationSwipe = localStorage.getItem('SessionNavigationSwipe') !== 'false';
-
-  @mobx.observable
-  enableNavigationTap = localStorage.getItem('SessionNavigationTap') !== 'false';
-
-  @mobx.observable
-  enableOptionAutohide = localStorage.getItem('SessionOptionAutohide') === 'true';
-
-  @mobx.observable
-  enableOptionRTL = localStorage.getItem('SessionOptionRTL') !== 'false';
+  optionRTL = localStorage.getItem('SessionOptionRTL') !== 'false';
 
   @mobx.observable
   pageSize = <app.PageSize> parseFloat(localStorage.getItem('SessionPageSize') || '0');
