33cd22979e025cec2258392654c6e7a01b24a806
Deathspike <meister.deathspike@outlook.com>
2020-04-10 13:56:06 +0200

    Add provider name to series list image

--- src/shared/components/SeriesListComponent.tsx
+++ src/shared/components/SeriesListComponent.tsx
@@ -19,8 +19,9 @@ export class SeriesListComponent<T extends app.ISeriesItem> extends app.BaseComp
         {this.props.series.length !== 0 && <mui.Grid className={`inset-bottom ${this.classes.seriesContent}`}>
           {this.props.series.map((series) => (
             <mui.Grid className={this.classes.series} key={getKey(series)} onClick={() => this.props.onClick(series)}>
-              <app.SeriesImage className={this.classes.image} offset={516} src={getUrl(series)} />
+              <app.SeriesImage className={this.classes.image} offset={516} src={getImage(series)} />
               <mui.Typography className={this.classes.title}>{series.title}</mui.Typography>
+              <mui.Typography className={this.classes.provider}>{getProvider(series)}</mui.Typography>
               {Boolean(series.unreadCount) && <mui.Typography className={this.classes.unreadCount}>{series.unreadCount}</mui.Typography>}
             </mui.Grid>
           ))}
@@ -40,13 +41,22 @@ function getKey(series: app.ISeriesItem) {
   }
 }
 
-function getUrl(series: app.ISeriesItem) {
+function getImage(series: app.ISeriesItem) {
   if (series.id) {
     return app.api.library.seriesImageUrl(series.id);
   } else if (series.imageId) {
     return app.api.remote.imageUrl(series.imageId);
   } else {
     throw new Error();
+  }
+}
+
+function getProvider(series: app.ISeriesItem) {
+  const match = series.url.match(/^(?:.+?):\/\/(.+?)\.(.+?)\//);
+  if (match && match[1] && match[2]) {
+    return ['com', 'net', 'org'].includes(match[2]) ? match[1] : `${match[1]}.${match[2]}`;
+  } else {
+    return series.url;
   }
 }
 
@@ -87,14 +97,23 @@ export const SeriesListComponentStyles = mui.createStyles({
     whiteSpace: 'nowrap',
     width: '100%'
   },
-  unreadCount: {
-    backgroundColor: app.theme.palette.primary.main,
+  provider: {
+    backgroundColor: app.theme.palette.primary.dark,
     color: app.theme.palette.primary.contrastText,
-    padding: '8px 0',
     position: 'absolute',
+    fontSize: '0.75rem',
     right: 0,
     textAlign: 'center',
     top: 0,
+    width: 44
+  },
+  unreadCount: {
+    backgroundColor: app.theme.palette.primary.light,
+    color: app.theme.palette.primary.contrastText,
+    position: 'absolute',
+    right: 0,
+    textAlign: 'center',
+    top: 18,
     width: 44
   }
 });
