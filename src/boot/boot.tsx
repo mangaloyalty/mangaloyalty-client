71b086baf3ef1cf6a9aaae1d110e58a756e987de
Deathspike <>
2020-05-01 20:34:13 +0200

    Rename `bootApp` to `boot`

--- src/boot/boot.tsx
+++ src/boot/boot.tsx
@@ -3,7 +3,7 @@ import * as areas from '..';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-export function bootApp(container: HTMLElement | null) {
+export function boot(container: HTMLElement | null) {
   areas.shared.connectStyles(areas);
   areas.shared.api.socket.attach();
   areas.shared.core.screen.openAsync(areas.library.MainController.createConstruct());
